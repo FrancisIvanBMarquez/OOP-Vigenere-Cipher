d7e2da58f82f18b91712859b52f7a826810d783f
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-04-22 05:53:16 +0800

    Pseudocode

--- OOP Vignere Cipher.py
+++ OOP Vignere Cipher.py
@@ -1,1 +1,9 @@
 # Vigenere Cipher_Marquez Francis Ivan B._BSCpE 1-5
+# Vigenere Cipher by BasselTech https://www.youtube.com/watch?v=m04k32-QaAc
+# adding characters
+# gettinhg positions
+# encrypt
+# decrypt
+# user input
+# output
+# additional
